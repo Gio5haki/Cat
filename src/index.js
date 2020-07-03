0ec1711fdc927fd8d8496387ddd7e869c51ebcd4
Giorgi Shakiashvili <giorgishakiashvili@gmail.com>
2020-07-03 20:37:53 +0400

    create class Cat with properties

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// Create an object that represents a cat. It should have properties for `tiredness`, `hunger`, `loneliness` and `happiness`
 
+class Cat {
+    constructor() {
+        this._tiredness = 0;
+        this._hunger = 0;
+        this._loneliness = 0;
+        this._hapiness = 0;
+    }
+}
+
+const Garfield = new Cat();
