1a7a8d883413ccd6e66d416d9609f62c9f58f046
Giorgi Shakiashvili <giorgishakiashvili@gmail.com>
2020-07-03 20:52:20 +0400

    add methods

--- src/index.js
+++ src/index.js
@@ -5,8 +5,43 @@ class Cat {
         this._tiredness = 0;
         this._hunger = 0;
         this._loneliness = 0;
-        this._hapiness = 0;
+        this._happiness = 0;
     }
+
+// Create methods that increase/decrease these properties
+    increaseTiredness(value) {
+        this._tiredness += value;
+    };
+    decreaseTiredness(value){
+        this._tiredness -= value;
+    };
+    
+    increaseHunger(value) {
+        this._hunger += value;
+    };
+
+    decreaseHunger(value) {
+        this._hunger -= value;
+    };
+    
+    increaseLoneliness(value) {
+        this._loneliness += value;
+    };
+
+    decreaseLoneliness(value) {
+        this._loneliness -= value;
+    };
+    
+    increaseHappiness(value) {
+        this._happiness += value;
+    };
+    decreaseHappiness(value) {
+        this._happiness -= value;
+    };
 }
+
+
+
+
 
 const Garfield = new Cat();
